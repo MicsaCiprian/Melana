9eae30e4c2007a1c388b6cd7a23fb1d1a4d9567e
Ciprian Micsa <ciprian.micsa@aninu.ro>
2023-10-30 12:12:38 +0200

    Initial Folder Structure

---
+++ config.php
@@ -0,0 +1,22 @@
+<?PHP
+	
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	
+	/** General Settings */
+	define("TITLE", "Melana");
+	define("TITLE_HOME", "Melana");
+	define("TITLE_ADMIN", "Melana Adm");
+	define("AUTHOR", "Melana");
+	define("DESCRIPTION", "Mel");
+	define("KEYWORDS", "");
+	define("COPYRIGHT", "2023");
+	define("URL", "");
+	define("URL_MAIN", "");
+	define("ICON", "");
+	define("LOGO", "");
+	define("LOGO_APP", "");
+	
+	/** Set TimeZone */
+	date_default_timezone_set("Europe/Bucharest");
---
+++ index.php
@@ -0,0 +1,4 @@
+<?PHP
+
+    /** Include Settings */
+    include("./config.php");
